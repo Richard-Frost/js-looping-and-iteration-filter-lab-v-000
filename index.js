5976611c94f2a56092e3663132af88950c90d65b
Richard Frost <frost.rchrd@gmail.com>
2019-01-15 17:00:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function findMatching(collection, name) {
 
 function findMatching1(collection, name) {
   let new = []
-  for (constant element of collection) {if 
+  for (constant driver of collection) {if driver.toLowerCase() === name.toLowerCase()) new.push(driver) }
+  return new
+}
 }
 
