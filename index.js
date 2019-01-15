7d43ddb87688dff30b0a42d5428bf282860bc7e2
Richard Frost <frost.rchrd@gmail.com>
2019-01-15 17:03:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function findMatching(collection, name) {
   let newDriver = []
-  for (const driver of collection) {if (driver.toLowerCase() === name.toLowerCase()) new.push(driver) }
+  for (const driver of collection) {if (driver.toLowerCase() === name.toLowerCase()) {new.push(driver)} }
   return newDriver
 }
 }
