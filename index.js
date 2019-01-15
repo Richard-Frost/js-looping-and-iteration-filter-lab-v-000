f5e796bc926464dbc0143a187a76275508ec23c4
Richard Frost <frost.rchrd@gmail.com>
2019-01-15 16:52:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file
+
+function findMatching(collection, name) {
+  return collection.filter(function(driver) { return driver.toLowerCase() === name.toLowerCase();
+  })l
+
+
