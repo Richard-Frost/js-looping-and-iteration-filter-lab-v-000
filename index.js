ff36e67a35465db018ca361cee667ea5f672789d
Richard Frost <frost.rchrd@gmail.com>
2019-01-15 17:59:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ function fuzzyMatch(collection, name) { return collection.filter(function(driver
 }
 
 function matchName(collection, name) {
-  return collection.filter(function(driver) { return driver.toLowerCase() === name.toLowerCase();
+  return collection.filter(function(driver) 
+  debugger;
+  
+  { return driver.toLowerCase() === name.toLowerCase();
   });
   
 }
